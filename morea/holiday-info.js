c751c83f252152fcf2670365f633b490665786d9
Andrea Jans <57602919+awjans@users.noreply.github.com>
2025-07-03 06:30:00 -1000

    Create holiday-info.js
    Moving Holiday Data under the Morea folder so that it doesn't get clobbered during an update from the Morea Template

---
+++ morea/holiday-info.js
@@ -0,0 +1,10 @@
+// Edit this file every semester and add the appropriate holidays
+var holidayData = {color: 'green',
+  events: [
+    // Starting Date for the Semester
+    {title: "Classes start", start: "2021-07-06"},
+    // Holidays within the Semester
+    {title: "Holiday: Independence Day (Observed)", start: "2021-07-05"},
+    // Ending Date for the Semester
+    {title: "Classes end", start: "2021-07-30"},
+  ]};
